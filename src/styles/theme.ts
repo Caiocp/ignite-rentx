29dbbb7d999efd2a148e42cdc24dc8f43204f198
caiocp <ccarvalho451@gmail.com>
2022-05-27 19:38:07 -0300

    fix theme property

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -23,7 +23,7 @@ export default {
     primary_400: 'Inter_400Regular',
     primary_500: 'Inter_500Medium',
     secondary_400: 'Archivo_400Regular',
-    secondary_700: 'Archivo_500Medium',
+    secondary_500: 'Archivo_500Medium',
     secondary_600: 'Archivo_600SemiBold',
   },
 };
