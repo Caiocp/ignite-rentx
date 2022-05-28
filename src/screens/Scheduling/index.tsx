7e30c080a70d31109d5badccc58b664ab6eb6ad6
caiocp <ccarvalho451@gmail.com>
2022-05-28 00:54:02 -0300

    add static scheduling screen

---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { BackButton } from '../../components/BackButton';
+
+import ArrowSvg from '../../assets/arrow.svg';
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer,
+} from './styles';
+import { StatusBar } from 'expo-status-bar';
+import { Button } from '../../components/Button';
+import { Calendar } from '../../components/Calendar';
+
+export function Scheduling() {
+  return (
+    <Container>
+      <StatusBar style="light" />
+      <Header>
+        <BackButton onPress={() => {}} color="shape" />
+        <Title>
+          Escolha uma{'\n'}
+          data de início e{'\n'}
+          fim do aluguel
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={false}>27/05/2022</DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue selected={false}>27/05/2022</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content>
+        <Calendar />
+      </Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+    </Container>
+  );
+}
